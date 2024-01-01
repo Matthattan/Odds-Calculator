bfaca725925a35c261e327c12292055442868f00
Matthattan <roshidridwan@gmail.com>
2024-01-01 16:32:48 +0000

    Commit Event Loomian fix
    - Moved some Loomians around
    - Fixed a few typos
    - readded some Loomians such as Rainbow Vari

--- Scripts/EventLoomians.js
+++ Scripts/EventLoomians.js
@@ -25,90 +25,101 @@ class Loomian {
       // set HTML box to disappear or smth
       gammaResult.style.display = "block";
       gammaResult.innerHTML = "Gamma: ";
+      rwResult.style.display = "block";
+      rwResult.innerHTML = "Rainbow Wisp: ";
     } else {
       gammaResult.style.display = "none";
+      rwResult.style.display = "none";
     }
   }
 }
 
-//name, odds, alphaG, gammaG, rW, increasedMultipliers, increasedOdds, alpha, gamma, gcharm, gboost
-const christmasGlassScorbs = new Loomian("Christmas Scorbs (Normal)", 12, true, false, false, false);
-const jellyGumpod = new Loomian("Jelly Gumpod (2022 Sweet Retreat)", 50, true, true, false, false);
-const lunarBunpuff = new Loomian("Lunar New Year Bunpuff (2023 Chinese New Year)", 60, true, true, false, false);
-const colouredGoppies = new Loomian("Coloured Goppies (2023 Loomunity)", 60, false, false, false, false);
-const valentinesAntsee = new Loomian("Valentines Antsee (2023 Valentines)", 80, true, true, false, false);
-const bronzeGeklow = new Loomian("Bronze Geklow (2020 Anniversary)", 100, true, false, true, false);
-const bronzeCopling = new Loomian("Bronze Copling (2023 Anniversary)", 100, true, false, false, true, false, 4096, 20480, 0.25, 0.015625);
-const sweetsPhancub = new Loomian("Sweets Phancub (2022 Sweet Retreat)", 100, true, true, true, false, false, 4096, 20480, 0.5, 0.0625);
-const snowmanTotemochi = new Loomian("Snowman Totemochi (2022 Christmas)", 150, true, false, false, false, 4096, 20480, 0.5, 0.0625);
-const cakeSlugling = new Loomian("Cake Slugling (2022 Sweet Retreat)", 200, true ,true ,true, false, false, 4096, 20480, 0.25, 0.015625);
-const silverGeklow = new Loomian("Silver Geklow (2020 Anniversary)", 250, true, false, false, true, false, 4096, 20480, 0.25, 0.015625);
-const silverCopling = new Loomian("Silver Copling (2023 Anniversary)", true, false, false, true, false, 4096, 20480, 0.25, 0.016625);
-const halloweenSlugling = new Loomian("Halloween Slugling (2019 Halloween)", 300, true, false, false, false, true, 4096, 20480, 0.5, 0.0625);
-const christmasGlassScorb = new Loomian("Glass Scorb (2022 Christmas)", 372, true, false, false, false, false, 4096, 20480, 0.5, 0.0625);
-const halloweenTaoshi = new Loomian("Halloween Taoshi (2022 Halloween)", 450, true, true, true, false, false, 4096, 20480, 0.5, 0.0625);
-const halloweenTerriti = new Loomian("Halloween Territi (2022 Halloween)", 450, true, true, true, false, false, 4096, 20480, 0.5, 0.0625);
-const goldGeklow = new Loomian("Gold Geklow (2020 Anniversary)", 500, true, false, false, true, false, 4096, 20480, 0.25, 0.015625);
-const goldCopling = new Loomian("Gold Copling (2023 Anniversary)", 500, true, false, false, true, false, 4096, 20480, 0.25, 0.015625);
-const christmasPropae = new Loomian("Santa Propae (2021 Christmas)", 500, true, true, true, false, false, 4096, 20480, 0.5, 0.0625);
-const halloweenShawchi = new Loomian("Halloween Shawchi (2019 Halloween)", 500, true, false, false, false, true, 4096, 20480, 0.5, 0.0625);
-const Icigool2020 = new Loomian("Coloured Icigool (2020 Christmas)", 600, true, true, true, true, false, true, 2048, 10240, 0.5, 0.0625);
-const Icigool2021 = new Loomian("Coloured Icigool (2021 Christmas and Beyond)", 600, true, true, true, false, false, 4096, 20480, 0.5, 0.0625);
-const christmasRagoon = new Loomian("Santa Ragoon (2019 Christmas)", 1000, true, false, false, false, false, 4096, 20480, 0.5, 0.0625);
-const rainbowShawchi = new Loomian("Rainbow Shawchi (2022 Loomunity)", 1000, true, true, true, false, true, 2048, 10240, 0.5, 0.0625);
-const rainbowFlorant = new Loomian("Rainbow Florant (2023 Loomunity)", 1000, true, true, true, false, false, 4096, 20480, 0.5, 0.00625);
-const emeraldGeklow = new Loomian("Emerald Geklow (2020 Anniversary)", 2000, true, false, false, true, false, 4096, 20480, 0.5, 0.015625);
-const emeraldCopling = new Loomian("Emerald Copling (2023 Anniversary)", 2000, true, false, false, true, false, 4096, 20480, 0.5, 0.015625);
-const rubyGeklow = new Loomian("Ruby Geklow (2020 Anniversary)", 5000, true, false, false, true, false, 4096, 20480, 0.5, 0.015625);
-const rubyCopling = new Loomian("Ruby Copling (2023 Anniversary)", 5000, true, false, false, true, false, 4096, 20480, 0.5, 0.015625);
-const normalKyeggo = new Loomian("Normal Kyeggo (2023 Easter)", (6*(100/37)), true, true, true, false, false, 4096, 20480, 0.5, 0.0625);
-const frillyKyeggo = new Loomian("Frilly Kyeggo (2023 Easter)", (6*(100/18.7)), true, true, true, false, false, 4096, 20480, 0.5, 0.625);
-const zzKyeggo = new Loomian("Zig-Zag Kyeggo (2023 Easter)", (6*(100/8.7)), true, true, true, false, false, 4096, 20480, 0.5, 0.625);
-const starKyeggo = new Loomian("Star Kyeggo (2023 Easter)", (6*(100/6)), true, true, true, false, false, 4096, 20480, 0.5, 0.0625);
-const fabergeKyeggo = new Loomian("Faberge Kyeggo (2023 Easter)", (6*(100/0.33)), true, true, true, false, false, 4096, 20480, 0.5, 0.150625);
-const sapphireGeklow = new Loomian("Sapphire Geklow (2020 Anniversary)", 10000, true, false, false, true, false, 4096, 20480, 0.5, 0.015625);
-const sapphireCopling = new Loomian("Sapphire Copling (2023 Anniversary)", 10000, true, false, false, true, false, 4096, 20480, 0.5, 0.015625);
-const mirrami = new Loomian("Mirrami (2023 Haunted Village)", 50, true, true, true, false, false, 4096, 20480, 0.5, 0.0625);
-const halloweenMistlebud = new Loomian("Halloween Mistlebud (2023 Haunted Village)", (100/1.6), true, true, true, false, false, 4096, 20480, 0.5, 0.0625);
-const halloweenCryocub = new Loomian("Halloween Cryocub (2023 Haunted Village", (100/0.3), true, true, true, false, false, 4096, 20480, 0.5, 0.0625);
+//name, odds, alphaG, gammaG
+const christmasGlassScorbs = new Loomian("Christmas Scorbs - Common (2022 Christmas)", 12, true, false);
+const jellyGumpod = new Loomian("Jelly Gumpod (2022 Sweet Retreat)", 50, true, true);
+const lunarBunpuff = new Loomian("Lunar New Year Bunpuff (2023 Chinese New Year)", 60, true, true);
+const colouredGoppies = new Loomian("Coloured Goppies (2023 Loomunity)", 60, false, false);
+const rainbowNymaurae = new Loomian("Rainbow Nymaurae (2023 Loomunity)", 15000, false, false);
+const rainbowPyramind = new Loomian("Rainbow Pyramind (2023 Loomunity)", 20000, false, false);
+const rainbowGoppie = new Loomian("Rainbow Goppie (2023 Loomunity)", 10000, false, false);
+const valentinesAntsee = new Loomian("Valentines Antsee (2023 Valentines)", 80, true, true);
+const bronzeGeklow = new Loomian("Bronze Geklow (2020 Anniversary)", 100, true, false);
+const bronzeCopling = new Loomian("Bronze Copling (2023 Anniversary)", 100, true, false);
+const sweetsPhancub = new Loomian("Sweets Phancub (2022 Sweet Retreat)", 100, true, true);
+const snowmanTotemochi = new Loomian("Snowman Totemochi (2022 Christmas)", 150, true, false);
+const cakeSlugling = new Loomian("Cake Slugling (2022 Sweet Retreat)", 200, true, true,);
+const silverGeklow = new Loomian("Silver Geklow (2020 Anniversary)", 250, true, false);
+const silverCopling = new Loomian("Silver Copling (2023 Anniversary)", true, false);
+const halloweenSlugling = new Loomian("Halloween Slugling (2019 Halloween)", 300, true, false);
+const christmasGlassScorb = new Loomian("Glass Scorb - Rare (2022 Christmas)", 372, true, false);
+const halloweenTaoshi = new Loomian("Halloween Taoshi (2022 Halloween)", 450, true, true);
+const halloweenTerriti = new Loomian("Halloween Territi (2022 Halloween)", 450, true, true);
+const goldGeklow = new Loomian("Gold Geklow (2020 Anniversary)", 500, true, false);
+const goldCopling = new Loomian("Gold Copling (2023 Anniversary)", 500, true, false);
+const christmasPropae = new Loomian("Santa Propae (2021 Christmas)", 500, true, true);
+const halloweenShawchi = new Loomian("Halloween Shawchi (2019 Halloween)", 500, true, false);
+const Icigool2020 = new Loomian("Coloured Icigool (2020 Christmas)", 600, true, true);
+const Icigool2021 = new Loomian("Coloured Icigool (2021 Christmas and Beyond)", 600, true, true);
+const christmasRagoon = new Loomian("Santa Ragoon (2019 Christmas)", 1000, true, false);
+const rainbowShawchi = new Loomian("Rainbow Shawchi (2022 Loomunity)", 1000, true, true);
+const rainbowVari = new Loomian("Rainbow Vari (2022 Loomunity)", 15000, false, false);
+const rainbowFlorant = new Loomian("Rainbow Florant (2023 Loomunity)", 1000, true, true);
+const emeraldGeklow = new Loomian("Emerald Geklow (2020 Anniversary)", 2000, true, false);
+const emeraldCopling = new Loomian("Emerald Copling (2023 Anniversary)", 2000, true, false);
+const rubyGeklow = new Loomian("Ruby Geklow (2020 Anniversary)", 5000, true, false);
+const rubyCopling = new Loomian("Ruby Copling (2023 Anniversary)", 5000, true, false);
+const normalKyeggo = new Loomian("Normal Kyeggo (2023 Easter)", (6*(100/37)), true, true);
+const frillyKyeggo = new Loomian("Frilly Kyeggo (2023 Easter)", (6*(100/18.7)), true, true);
+const zzKyeggo = new Loomian("Zig-Zag Kyeggo (2023 Easter)", (6*(100/8.7)), true, true);
+const starKyeggo = new Loomian("Star Kyeggo (2023 Easter)", (6*(100/6)), true, true);
+const fabergeKyeggo = new Loomian("Faberge Kyeggo (2023 Easter)", (6*(100/0.33)), true, true);
+const sapphireGeklow = new Loomian("Sapphire Geklow (2020 Anniversary)", 10000, true, false);
+const sapphireCopling = new Loomian("Sapphire Copling (2023 Anniversary)", 10000, true, false);
+const mirrami = new Loomian("Mirrami (2023 Haunted Village)", 50, true, true);
+const halloweenMistlebud = new Loomian("Halloween Mistlebud (2023 Haunted Village)", (100/1.6), true, true);
+const halloweenCryocub = new Loomian("Halloween Cryocub (2023 Haunted Village)", (100/0.3), true, true);
 
 // collate all objects into an array
 const optionData = [
-  sweetsPhancub,
-  cakeSlugling,
-  lunarBunpuff,
-  jellyGumpod,
-  valentinesAntsee,
+  halloweenSlugling,
+  halloweenShawchi,
+  christmasRagoon,
   bronzeGeklow,
   silverGeklow,
   goldGeklow,
   emeraldGeklow,
   rubyGeklow,
   sapphireGeklow,
-  bronzeCopling,
-  silverGeklow,
-  goldCopling,
-  emeraldCopling,
-  rubyCopling,
-  sapphireCopling,
-  halloweenSlugling,
-  halloweenShawchi,
-  christmasGlassScorb,
-  christmasGlassScorbs,
+  rainbowShawchi,
+  rainbowVari,
+  sweetsPhancub,
+  cakeSlugling,
+  jellyGumpod,
   halloweenTaoshi,
   halloweenTerriti,
   Icigool2020,
-  Icigool2021,
   christmasPropae,
+  Icigool2021,
+  christmasGlassScorb,
+  christmasGlassScorbs,
   snowmanTotemochi,
-  christmasRagoon,
-  rainbowShawchi,
-  rainbowFlorant,
+  lunarBunpuff,
+  valentinesAntsee,
   normalKyeggo,
   frillyKyeggo,
   zzKyeggo,
   starKyeggo,
   fabergeKyeggo,
+  rainbowFlorant,
+  rainbowNymaurae,
+  rainbowPyramind,
+  rainbowGoppie,
+  bronzeCopling,
+  silverCopling,
+  goldCopling,
+  emeraldCopling,
+  rubyCopling,
+  sapphireCopling,
   mirrami,
   halloweenMistlebud,
   halloweenCryocub
